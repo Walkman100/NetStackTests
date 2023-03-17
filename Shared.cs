96e9d5120fb956929060869879f58f69dc5a43a7
░▒▓█│【Walkman】│█▓▒░ <MatthewC@direwolf.co.za>
2023-03-17 12:46:12 +0200

    moved preprocessor compile tests to shared

--- Shared.cs
+++ Shared.cs
@@ -5,4 +5,40 @@ public static class Shared {
         var frameworkName = new System.Runtime.Versioning.FrameworkName(AppDomain.CurrentDomain.SetupInformation.TargetFrameworkName);
         return frameworkName.Version;
     }
+
+    public static void WriteCompileTests() {
+#if NETCOREAPP
+        Console.WriteLine("NetCore");
+#endif
+#if NET
+        Console.WriteLine("Net??");
+#endif
+#if NETCOREAPP1_0_OR_GREATER
+        Console.WriteLine("NetCore 1.0+");
+#endif
+#if NETCOREAPP1_1_OR_GREATER
+        Console.WriteLine("NetCore 1.1+");
+#endif
+#if NETCOREAPP2_0_OR_GREATER
+        Console.WriteLine("NetCore 2.0+");
+#endif
+#if NETCOREAPP2_1_OR_GREATER
+        Console.WriteLine("NetCore 2.1+");
+#endif
+#if NETCOREAPP2_2_OR_GREATER
+        Console.WriteLine("NetCore 2.2+");
+#endif
+#if NETCOREAPP3_0_OR_GREATER
+        Console.WriteLine("NetCore 3.0+");
+#endif
+#if NETCOREAPP3_1_OR_GREATER
+        Console.WriteLine("NetCore 3.1+");
+#endif
+#if NET5_0
+            Console.WriteLine("NetCore 5.0");
+#endif
+#if NET5_0_OR_GREATER
+        Console.WriteLine("NetCore 5.0+");
+#endif
+    }
 }
